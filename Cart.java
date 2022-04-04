4e996838b19cf3d825de5af42284f39ebcc40637
amberkahklen <99503160+amberkahklen@users.noreply.github.com>
2022-04-03 19:48:13 -0700

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -3,60 +3,79 @@ import java.util.ArrayList;
 
 public class Cart {
 	
-	private ArrayList<MenuItem> itemsInCart;
+	private ArrayList<MenuItem> itemsInCart = new ArrayList<MenuItem>(100);
 
-private ArrayList<MenuItem> cartList()	//all instances of menuItem are a dummy class
-//I created as a placeholder until I can use the actual menuItem
-{
-	
-	
-	return itemsInCart; //in progress there might be a better way to do this
-}
-
-public boolean addItem(MenuItem item)
-{
-	itemsInCart.add(item);
-	
-	return true;
-}
-public boolean removeItem(MenuItem item)
-{
-	for(int i = 0; i < itemsInCart.size(); i++)
+	public ArrayList<MenuItem> cartList()	//all instances of menuItem are a dummy class
+	//I created as a placeholder until I can use the actual menuItem
 	{
-		if(itemsInCart.get(i).getName().equals(item.getName()))
+		
+		
+		return itemsInCart; //in progress there might be a better way to do this
+	}
+	
+	public boolean addItem(MenuItem item)
+	{
+		itemsInCart.add(item);
+		
+		return true;
+	}
+	
+	public boolean removeItem(MenuItem item)
+	{
+		if(!(itemsInCart.isEmpty()))
 		{
-			itemsInCart.remove(i);
-			return true;
+			for(int i = 0; i < itemsInCart.size(); i++)
+			{
+				if(itemsInCart.get(i).getName().equals(item.getName()))
+				{
+					itemsInCart.remove(i);
+					return true;
+				}
+			}
 		}
+		
+		return false;
 	}
 	
-	return false;
-}
-
-public double showTotal()
-{
-	MenuItem temp;
-	double total = 0;
-	
-	for(int j = 0; j < itemsInCart.size(); j++) //need menuItem class
+	public int findItem(MenuItem item)
 	{
-		temp = itemsInCart.get(j);
-		total = total + temp.getPrice();
+		int numItem = 0;
+		
+		for(int k = 0; k < itemsInCart.size(); k++)
+		{
+			if(itemsInCart.get(k).getName().equals(item.getName()))
+			{
+				numItem++;
+			}
+		}
+		
+		return numItem;
 	}
-	return total;
-}
-
-public void checkout()
-{
-	//I'm not sure what this is used for
 	
-}
-
-public int numbercartItems()
-{
-	int totalitems = itemsInCart.size();
+	public double showTotal()
+	{
+		MenuItem temp;
+		double total = 0;
+		
+		for(int j = 0; j < itemsInCart.size(); j++) //need menuItem class
+		{
+			temp = itemsInCart.get(j);
+			total = total + temp.getPrice();
+		}
+		return total;
+	}
 	
-	return totalitems;
-}
+	public void checkout()
+	{
+		//I'm not sure what this is used for
+		
+	}
+	
+	public int numbercartItems()
+	{
+		int totalitems = itemsInCart.size();
+		
+		return totalitems;
+	}
 
 }
